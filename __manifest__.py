5cc63dbe947c1ecfe5a0e8b0f6fadd82dee6264f
ahmed youssef <ayopopa36@gmail.com>
2022-06-17 07:00:51 +0200

    doctor patient appointment working

---
+++ __manifest__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "hospital management ",
+
+    'summary': "hospital management system for all hospitalllll lllll",
+
+    'description': "hospital management system",
+    'sequence':-100,
+ 
+
+    'author': "abyo Company",
+    'website': "http://www.abyo.com",
+
+ 
+    # for the full list
+    'category': 'hospital',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['sale','base','mail','product'],
+
+    # always loaded
+    'data': [
+        # security
+        'security/ir.model.access.csv',
+
+        # data
+        'data/data.xml',
+    
+        # views
+        'views/patient.xml',
+        'views/menu.xml',
+        
+   
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
